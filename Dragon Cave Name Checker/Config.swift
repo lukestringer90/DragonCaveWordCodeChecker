0681e9d4c91494e5a5fb1bf5b56f2cc6a2eff198
Luke Stringer <luke.stringer@3squared.com>
2017-07-23 15:23:50 +0100

    Add min and max token length, load scroll html in

---
+++ Dragon Cave Name Checker/Config.swift
@@ -0,0 +1,22 @@
+//
+//  Config.swift
+//  Dragon Cave Name Checker
+//
+//  Created by Luke Stringer on 23/07/2017.
+//  Copyright © 2017 3Squared. All rights reserved.
+//
+
+import Foundation
+
+enum Config {
+    
+    enum TokenLength: Int {
+        case minimum = 3
+        case maximum = 5
+        
+        static var range: CountableClosedRange<Int> {
+            return TokenLength.minimum.rawValue...TokenLength.maximum.rawValue
+        }
+    }
+    
+}
