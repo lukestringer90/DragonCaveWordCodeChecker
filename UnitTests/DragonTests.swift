86d862e924af5c34109ab8ef3dbba4a179131c2a
Luke Stringer <luke@lukestringer.me>
2017-07-27 19:55:42 +0100

    Add unit tests target

---
+++ UnitTests/DragonTests.swift
@@ -0,0 +1,13 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Luke Stringer on 27/07/2017.
+//  Copyright © 2017 3Squared. All rights reserved.
+//
+
+import XCTest
+
+class DragonTests: XCTestCase {
+    
+}
